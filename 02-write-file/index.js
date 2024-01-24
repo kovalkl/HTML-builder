8a21f07376e2ac94785022871011189fc1d09665
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-01-24 17:19:10 +0300

    feat: add 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout, exit } = process;
-
 const pathToFile = path.join(__dirname, 'text.txt');
 const ws = fs.createWriteStream(pathToFile);
-stdout.write('Write your text\n');
-stdin.on('data', (data) => {
-  if (data.toString().trim() === 'exit') {
-    finishProgram();
-  } else {
-    ws.write(data);
-  }
-});
-
-process.on('SIGINT', () => finishProgram());
-
 const finishProgram = () => {
   stdout.write('Good buy!');
   exit();
 };
+stdout.write('Write your text\n');
+stdin.on('data', (data) => {
+  data.toString().trim() === 'exit' ? finishProgram() : ws.write(data)
+});
+process.on('SIGINT', () => finishProgram());
